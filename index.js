cc97911f9fd94f3668a390d603046ba22eca9c2f
Andrew Kairys <andrewkairys@gmail.com>
2017-10-15 23:27:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ function sayHiToGrandma(string) {
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase())
   return "I can\'t hear you!"
-  if (string === string.toUpperCase())
-  return "YES INDEED!""
-  if (string === "I love you, Grandma""
+  else if (string === string.toUpperCase())
+  return "YES INDEED!"
+  else if (string === "I love you, Grandma"
   return "I love you, too"
 }
